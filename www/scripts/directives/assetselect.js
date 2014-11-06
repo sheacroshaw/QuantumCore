800d5e14d00a3bcd069baff1a6d14cadc1873189
shea <shea@laptopOne.(none)>
2014-11-06 13:52:41 -0600

    fixed Add custom asset add to scope

--- www/scripts/directives/assetselect.js
+++ www/scripts/directives/assetselect.js
@@ -8,10 +8,14 @@ angular.module('quantumRApp')
       link: function postLink(scope, element, attrs) {
 
         var asset = function(){
-  	    	this.name = null;
-  	    	this.unit = null;
-  	    	this.hours = null;
-  	    };
+        this.name = null;
+        this.unit = null;
+        this.hours = null;
+        this.Fuel_Input = 0;
+        this.Water_Input = 0;
+        this.Rental = 0;
+        this.Unit_Number = null;
+    };
 
   	    scope.addCustomAsset = function () {
   	    	scope.customAssets.push(new asset());
